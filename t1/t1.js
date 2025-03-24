9b4e50226d79e0b0f2f7e010b26d114552555eba
nealukumies <nea.lukumies@metropolia.fi>
2025-03-24 10:31:38 +0200

    task1

--- t1/t1.js
+++ t1/t1.js
@@ -1,5 +1,5 @@
 // array for todo list
-const todoList = [
+const toDoList = [
   {
     id: 1,
     task: 'Learn HTML',
@@ -26,5 +26,35 @@ const todoList = [
     completed: false,
   },
 ];
+/*<li>
+   <input type="checkbox" id="todo-1" checked>
+   <label for="todo-1">Buy milk</label>
+</li>
+<li>
+   <input type="checkbox" id="todo-2">
+   <label for="todo-2">Buy eggs</label>
+</li>
+<li>
+   <input type="checkbox" id="todo-3" >
+   <label for="todo-3">Buy bread</label>
+</li>
+note: if completed property is true, the checkbox should be automatically checked.
+*/
+const ul = document.querySelector('ul');
+for (let i = 0; i < toDoList.length; i++) {
+  const li = document.createElement('li');
+  const taskId = 'todo-' + (i + 1);
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', taskId);
+  if (toDoList[i].completed) {
+    input.checked = true;
+  }
+  li.appendChild(input);
+  const label = document.createElement('label');
+  label.setAttribute('for', taskId);
+  label.innerHTML = toDoList[i].task;
+  li.appendChild(label);
 
-// add your code here
+  ul.insertAdjacentElement('beforeend', li);
+}
